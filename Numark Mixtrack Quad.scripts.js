bac551c50a58ab46ebe4331778a05f84a818d8cc
KWKSND <33409134+KWKSND@users.noreply.github.com>
2022-12-06 17:03:29 -0700

    Add files via upload

--- Numark Mixtrack Quad.scripts.js
+++ Numark Mixtrack Quad.scripts.js
@@ -15,49 +15,49 @@
 //- Updated on 12/6/2022 by DJ KWKSND (changed a bunch of code and mappings)
 //- https://github.com/KWKSND/Numark-Mixtrack-Quad-4-Deck-mapping-for-Mixxx-2.3.3
 //- I agree with the above statement.
-//- I had not worked with midi or javascript, still more done in the last week or so than all you in 12 years ROFL
 //- You guys drove my O.C.D. crazy enough to get it done for everyone thanks for the inspiration.
+//- I had not worked with midi or javascript, still more done in the last week or so than all you in 12 years ROFL
 //- I hope many people get to enjoy this wonderful controller for years to come without being robbed by VDJ
 //-
 //- Whats New?
-//- Incorrectly mapped buttons were found and fixed
-//- FX123 & Filter knob speed is fixed
-//- Pressing shift + turning FX123 knobs now change what effects are assigned to the pads
-//- Pressing shift + keylock now enables / disables keylock
-//- Pressing shift + Range now scales the range of the pitch slider
-//- Soft takeover added to all sliders and knobs, so there is no more extreme jumps in volumes or filters etc
-//- Jogwheel direction when not in scratch mode was wrong now fixed
-//- Fixed some timer kill errors
-//- AutoDJ now auto enabled with nice slow fade in when Mixxx starts so you can start Mixxx and walk away
-//- New colors on most pads
-//- Beautiful LED intro on Mixxx startup
-//- Idle mode added to keep the LED show going until you start DJing, also now resumes if idle again
-//- Improved controls and LED animation with scratching especially in reverse
-//- LEDs now work with scratching and stop with end of track
-//- Added master clipping indicators to the folder/file LEDs to keep you from blowing your shit up
-//- Added channel clipping indicators to the headphone LEDs for each channel to prevent overdoing it on the EQ
-//- Shutdown function added to turn off all possible LEDs with Mixxx app shutdown
-//- Stutter flashes the BPM and Cue flashes to indicate 30 seconds from end of track
-//- And finally i get to announce all jogwheel LEDs now work without having to press play on the controller :D
-//- Hit play on the controller, or with the mouse on the Mixxx app, or let AutoDJ do it for you it all works YES!!!
+//-  Incorrectly mapped buttons were found and fixed
+//-  FX123 & Filter knob speed is fixed
+//-  Pressing shift + turning FX123 knobs now change what effects are assigned to the pads
+//-  Pressing shift + keylock now enables / disables keylock
+//-  Pressing shift + Range now scales the range of the pitch slider
+//-  Soft takeover added to all sliders and knobs, so there is no more extreme jumps in volumes or filters etc
+//-  Jogwheel direction when not in scratch mode was wrong now fixed
+//-  Fixed some timer kill errors
+//-  AutoDJ now auto enabled with nice slow fade in when Mixxx starts so you can start Mixxx and walk away
+//-  New colors on most pads
+//-  Beautiful LED intro on Mixxx startup
+//-  Idle mode added to keep the LED show going until you start DJing, also now resumes if idle again
+//-  Improved controls and LED animation with scratching especially in reverse
+//-  LEDs now work with scratching and stop with end of track
+//-  Added master clipping indicators to the folder/file LEDs to keep you from blowing your shit up
+//-  Added channel clipping indicators to the headphone LEDs for each channel to prevent overdoing it on the EQ
+//-  Shutdown function added to turn off all possible LEDs with Mixxx app shutdown
+//-  Stutter flashes the BPM and Cue flashes to indicate 30 seconds from end of track
+//-  And finally i get to announce all jogwheel LEDs now work without having to press play on the controller :D
+//-  Hit play on the controller, or with the mouse on the Mixxx app, or let AutoDJ do it for you it all works YES!!!
 //-
 //- Features:
-//- Supports 4 decks
-//- Library browse knob + load A/B
-//- Channel volume, cross fader, cue gain / mix, Master gain, filters, pitch and pitch bend
-//- Scratch/CD mode toggle
-//- Headphone output toggle
-//- Samples (Using 16 samples)
-//- Effects (Using 4 effect units)
-//- Cue 1-4 hot cues
-//- Loops:
-//- 	Loop in (Loop halves)
-//- 	Loop out (Loop doubles)
-//- 	Re-loop (Starts loop at current playback point)
-//- 	Loop Delete (Deactivates loop)
+//-  Supports 4 decks
+//-  Library browse knob + load A/B
+//-  Channel volume, cross fader, cue gain / mix, master gain, filters, pitch and pitch bend
+//-  Scratch/CD mode toggle
+//-  Headphone output toggle
+//-  Samples (Using 16 samples)
+//-  Effects (Using 4 effect units)
+//-  Cue 1-4 hot cues
+//-  Loops:
+//-   Loop in (Loop halves)
+//-   Loop out (Loop doubles)
+//-   Re-loop (Starts loop at current playback point)
+//-   Loop Delete (Deactivates loop)
 //-
 //- Known Bugs:
-//- Each slide/knob needs to be moved on Mixxx startup to match levels with the Mixxx UI
+//-  Each slide/knob needs to be moved on Mixxx startup to match levels with the Mixxx UI
 
 function NumarkMixTrackQuad() {}
 
